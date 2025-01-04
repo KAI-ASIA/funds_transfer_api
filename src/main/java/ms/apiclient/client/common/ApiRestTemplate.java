05dc4c46e8694d2ff644dadc8d1ab2953fe2ef60
lamnt2501 <trung.lam.for.work@gmail.com>
2025-01-04 23:18:00 +0700

    update client

--- src/main/java/ms/apiclient/client/common/ApiRestTemplate.java
+++ src/main/java/ms/apiclient/client/common/ApiRestTemplate.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,27 +18,20 @@ import ms.apiclient.model.ApiResponse;
 @Component
 public class ApiRestTemplate {
 
-	 public ApiResponse callApi(ApiRequest apiReq, String url, int apiTimeout) throws Exception {
-		 RestTemplate  restTemplate = new RestTemplate();
-		 HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+    public ApiResponse callApi(ApiRequest apiReq, String url, int apiTimeout) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
         clientHttpRequestFactory.setConnectTimeout(apiTimeout);
         clientHttpRequestFactory.setReadTimeout(apiTimeout);
         restTemplate.setRequestFactory(clientHttpRequestFactory);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity<ApiRequest> req = new HttpEntity<ApiRequest>(apiReq, headers);
-        ResponseEntity<ApiResponse> result;
+        HttpEntity<ApiRequest> req = new HttpEntity<>(apiReq, headers);
         try {
-            result = restTemplate.postForEntity(url, req, ApiResponse.class);
+            return restTemplate.postForObject(url, req, ApiResponse.class);
         } catch (Exception restEx) {
             throw new Exception("callRestAPISync_" + apiReq.getHeader().getApi(), restEx);
         }
-        ApiResponse apiRes = result.getBody();
-        return apiRes;
- }
-	
-	
-	
+    }
 }
