d0b57cd20857a5a9ab031f9c625f4d469beaed16
Phạm Huy Hoàng <hoangphamhuy275132@gmail.com>
2025-01-05 15:35:40 +0700

    update authen

--- src/main/java/ms/apiclient/authen/AuthenClient.java
+++ src/main/java/ms/apiclient/authen/AuthenClient.java
@@ -4,18 +4,62 @@ import ms.apiclient.client.common.ApiCallClient;
 import ms.apiclient.model.ApiHeader;
 import ms.apiclient.model.ApiRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConfigurationProperties(prefix = "dev-api")
 public class AuthenClient extends ApiCallClient {
-	
-	@Autowired
-	private ApiCallClient CallApiHelper;
-	
-	public LoginResult login(String location, AuthRequest t24request, ApiHeader header){
-		ApiRequest apiReq = buildENQUIRY(t24request, header);
-		return this.call(location, apiReq, LoginResult.class);
+	/**
+	 * Phương thức này dùng để gọi đến Auth-0
+	 * @param location - Vị trí truy vết để ghi log.
+	 * @param authRequest - Data cần truyền vào (Username, Password, LoginTime).
+	 * @param header - Header của request đầu vào.
+	 * @return Trả về AuthLoginResponse chứa (ResponseCode, SessionId, Username, TransId, PackageUser, Phone, CustomerID, CustomerName, CompanyName) hoặc 1 ApiError
+	 */
+	public AuthLoginResponse getLogin(String location, AuthRequest authRequest, ApiHeader header){
+		authRequest.setAuthenType("getLogin");
+		ApiRequest apiReq = buildENQUIRY(authRequest, header);
+		return this.call(location, apiReq, AuthLoginResponse.class);
 	}
 
+	/**
+	 * Phương thức này dùng để gọi đến Auth-1
+	 * @param location - Vị trí truy vết để ghi log.
+	 * @param authRequest - Data cần truyền vào (Chỉ cần sessionId).
+	 * @param header - Header của request đầu vào.
+	 * @return Trả về AuthLoginResponse chứa (ResponseCode, SessionId, Username) hoặc 1 ApiError
+	 */
+	public AuthLoginResponse takeSession(String location, AuthRequest authRequest, ApiHeader header){
+		authRequest.setAuthenType("takeSession");
+		ApiRequest apiReq = buildENQUIRY(authRequest, header);
+		return this.call(location, apiReq, AuthLoginResponse.class);
+	}
+
+	/**
+	 * Phương thức này dùng để gọi đến Auth-2
+	 * @param location - Vị trí truy vết để ghi log.
+	 * @param authRequest - Data cần truyền vào (Username, SessionId, Gmail, TransTime, TransId, TransInfo, SmsParams).
+	 * @param header - Header của request đầu vào.
+	 * @return Trả về AuthLoginResponse chứa (ResponseCode, TransId) hoặc 1 ApiError
+	 */
+	public AuthLoginResponse getOTP(String location, AuthRequest authRequest, ApiHeader header){
+		authRequest.setAuthenType("getOTP");
+		ApiRequest apiReq = buildENQUIRY(authRequest, header);
+		return this.call(location, apiReq, AuthLoginResponse.class);
+	}
+
+
+	/**
+	 * Phương thức này dùng để gọi đến Auth-3
+	 * @param location - Vị trí truy vết để ghi log.
+	 * @param authRequest - Data cần truyền vào (Username, SessionId, OTP, TransTime, TransId).
+	 * @param header - Header của request đầu vào.
+	 * @return Trả về AuthLoginResponse chứa (ResponseCode, TransId) hoặc 1 ApiError
+	 */
+	public AuthLoginResponse confirmOTP(String location, AuthRequest authRequest, ApiHeader header){
+		authRequest.setAuthenType("confirmOTP");
+		ApiRequest apiReq = buildENQUIRY(authRequest, header);
+		return this.call(location, apiReq, AuthLoginResponse.class);
+	}
 }
--- src/main/java/ms/apiclient/t24util/T24Request.java
+++ src/main/java/ms/apiclient/t24util/T24Request.java
@@ -2,10 +2,12 @@ package ms.apiclient.t24util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Builder;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
+@Builder
 public class T24Request {
     private String authenType;
     private String username;
