be8439a03085dea3d194573b2ca8cb9fd77348dc
lamnt2501 <trung.lam.for.work@gmail.com>
2025-01-19 16:37:04 +0700

    refactor[job] refactor job

--- src/main/java/com/kaiasia/app/service/fundstransfer/job/ConsolidationFTQueue.java
+++ src/main/java/com/kaiasia/app/service/fundstransfer/job/ConsolidationFTQueue.java
@@ -2,20 +2,22 @@ package com.kaiasia.app.service.fundstransfer.job;
 
 import com.kaiasia.app.service.fundstransfer.model.entity.TransactionInfo;
 import com.kaiasia.app.service.fundstransfer.utils.ObjectAndJsonUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConsolidationFTQueue {
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
     @Value("${spring.redis.queueName}")
     private String queueName;
 
-    public Long addToQueue(TransactionInfo transactionInfo) {
-        return redisTemplate.opsForList().leftPush(queueName, ObjectAndJsonUtils.toJson(transactionInfo));
+    public ConsolidationFTQueue(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void addToQueue(TransactionInfo transactionInfo) {
+        redisTemplate.opsForList().leftPush(queueName, ObjectAndJsonUtils.toJson(transactionInfo));
     }
 
     public TransactionInfo getFromQueue() {
